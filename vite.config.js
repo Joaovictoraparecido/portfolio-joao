07c2d6410e35d0b4043b48ca1f5e21d9f2d43eae
JOAO VICTOR APARECIDO <115484907+Joaovictoraparecido@users.noreply.github.com>
2025-08-20 18:30:57 -0300

    1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-joao/', // 👈 nome exato do repo
+  base: 'portfolio-jv', // 👈 nome exato do repo
 })
